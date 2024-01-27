ba744e9afae36ba2e8020e72aafd03d5331d2a09
Himanshu <himansingh24@gmail.com>
2024-01-27 21:15:58 +0530

    feat: add support for stats API

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,107 @@ app.get('/transactions', async (req, res) => {
   res.json({ "response": transactions })
 })
 
+app.get('/stats', async (req, res) => {
+  const month = parseInt(req.query.month) || 3;
+  const type = req.query.type;
+  const matchMonth = { $expr: { $eq: [{ "$month": "$dateOfSale" }, month] } }
+  if ("text" === type) {
+    res.json({
+      "response": {
+        ...await getTextStats(matchMonth)
+      }
+    })
+  } else if ("bar" === type) {
+    res.json({
+      "repsonse": {
+        ...await getBarStats(matchMonth)
+      }
+    })
+  } else if ("pie" === type) {
+    res.json({
+      "response": {
+        ...await getPieStats(matchMonth)
+      },
+    })
+  } else {
+    const textResponse = await getTextStats(matchMonth);
+    const barResponse = await getBarStats(matchMonth);
+    const pieResposne = await getPieStats(matchMonth);
+    res.json({
+      "response": {
+        "textResponse": textResponse,
+        "barResponse": barResponse,
+        "pieResponse": pieResposne
+      }
+    });
+  }
+})
+
+const getTextStats = async (matchMonth) => {
+  // TODO: try/catch block and handle errors
+  const totalSalesByMonth = await Transactions.aggregate([
+    { $match: matchMonth },
+    { $project: { price: 1, category: 1 } },
+    { $group: { _id: '$category', totalSales: { $sum: "$price" } } },
+    { $project: { "category": "$_id", "totalSales": 1, _id: 0 } }
+  ])
+
+  let itemsSoldByMonth = await Transactions.aggregate([
+    { $match: { "sold": true, ...matchMonth } },
+    { $project: { sold: 1 } },
+    { $group: { _id: "$sold", totalCount: { $sum: 1 } } },
+    { $project: { totalCount: 1, _id: 0 } }
+  ])
+  if (itemsSoldByMonth) {
+    itemsSoldByMonth = itemsSoldByMonth[0]["totalCount"];
+  }
+
+  let itemsNotSoldByMonth = await Transactions.aggregate([
+    { $match: { "sold": false, ...matchMonth } },
+    { $project: { sold: 1 } },
+    { $group: { _id: "$sold", totalCount: { $sum: 1 } } },
+    { $project: { totalCount: 1, _id: 0 } },
+  ])
+  if (itemsNotSoldByMonth) {
+    itemsNotSoldByMonth = itemsNotSoldByMonth[0]["totalCount"];
+  }
+  return {
+    "totalSalesByMonth": totalSalesByMonth,
+    "itemSoldByMonth": itemsSoldByMonth,
+    "itemsNotSoldByMonth": itemsNotSoldByMonth
+  }
+};
+
+const getBarStats = async (matchMonth) => {
+  const priceAggregate = { $group: { _id: { $switch: { branches: [{ case: { $and: [{ $gte: ["$price", 0] }, { $lte: ["$price", 100] }] }, then: "0-100" }, { case: { $and: [{ $gte: ["$price", 101] }, { $lte: ["$price", 200] }] }, then: "101-200" }, { case: { $and: [{ $gte: ["$price", 201] }, { $lte: ["$price", 300] }] }, then: "201-300" }, { case: { $and: [{ $gte: ["$price", 301] }, { $lte: ["$price", 400] }] }, then: "301-400" }, { case: { $and: [{ $gte: ["$price", 401] }, { $lte: ["$price", 500] }] }, then: "401-500" }, { case: { $and: [{ $gte: ["$price", 501] }, { $lte: ["$price", 600] }] }, then: "501-600" }, { case: { $and: [{ $gte: ["$price", 601] }, { $lte: ["$price", 700] }] }, then: "601-700" }, { case: { $and: [{ $gte: ["$price", 701] }, { $lte: ["$price", 800] }] }, then: "701-800" }, { case: { $and: [{ $gte: ["$price", 801] }, { $lte: ["$price", 900] }] }, then: "801-900" }, { case: { $gte: ["$price", 901] }, then: "901-above" }], default: "Unknown" } }, count: { $sum: 1 } } };
+  let priceAggregationResult = await Transactions.aggregate([
+    { $match: matchMonth },
+    priceAggregate
+  ]);
+  let priceAggregationResponse = {};
+  for (const key in priceAggregationResult) {
+    const range = priceAggregationResult[key]["_id"];
+    const count = priceAggregationResult[key]["count"];
+    priceAggregationResponse[range] = count;
+  }
+  return priceAggregationResponse;
+}
+
+const getPieStats = async (matchMonth) => {
+  const pieResult = await Transactions.aggregate([
+    { $match: matchMonth },
+    { $project: { category: 1 } },
+    { $group: { _id: "$category", totalCount: { $sum: 1 } } },
+  ]);
+  let pieResponse = {};
+  for (const key in pieResult) {
+    const category = pieResult[key]["_id"];
+    const count = pieResult[key]["totalCount"];
+    pieResponse[category] = count;
+  }
+  return pieResponse;
+};
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
